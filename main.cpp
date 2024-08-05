ad18fd0b593a876eb2bef2c78b9593831657c180
Mohamed labbit <151045562+Mohammed0101-lgtm@users.noreply.github.com>
2024-08-05 03:41:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,39 +5,31 @@
 
 int main(void) {
     in_out input;
-    std::cout << "+-------------+" << '\n';
-    std::cout << "| Bank System |"  << '\n'; 
-    std::cout << "+-------------+" << '\n';
-    std::cout << "1. Sign up" << '\n';
-    std::cout << "2. Log in" << '\n';
-    std::cout << "3. Show account list" << '\n';
-    std::cout << "4. Search account" << '\n';
-    std::cout << "0. Exit" << '\n';
-    std::cout << "---------------" << '\n';
+    std::cout <<  "+---------------+"    << '\n';
+    std::cout <<  "|  Bank System  |"    << '\n'; 
+    std::cout <<  "+---------------+"    << '\n';
+    std::cout <<  "1. Sign up"           << '\n';
+    std::cout <<  "2. Log in"            << '\n';
+    std::cout <<  "3. Show account list" << '\n';
+    std::cout <<  "4. Search account"    << '\n';
+    std::cout <<  "0. Exit"              << '\n';
+    std::cout <<  "+---------------+"    << '\n';
 
     unsigned int choice;
     std::cout << "Enter choice: ";
-    std::cin >> choice;
+    std::cin  >> choice;
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     while (choice < 0 || choice > 5) {
         std::cout << "Unavailable choice, try again: ";
-        std::cin >> choice;
+        std::cin  >> choice;
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
 
     switch (choice) {
-        case 1:
-            input.create_acc();
-            break;
-        case 2:
-            input.login();
-            break;
-        case 3:
-            input.display_acc_list();
-            break;
-        case 4:
-            input.search_acc();
-            break;
+        case 1: input.create_acc();       break;
+        case 2: input.login();            break;
+        case 3: input.display_acc_list(); break;
+        case 4: input.search_acc();       break;
         case 0:
             std::cout << "Program exited!" << std::endl;
             break;
