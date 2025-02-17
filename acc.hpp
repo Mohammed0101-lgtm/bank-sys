0fddff068e8fc21d68583686b879770b25379995
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-02-17 01:16:28 +0100

    stuff

--- acc.hpp
+++ acc.hpp
@@ -6,9 +6,9 @@
 const std::string Account_dir = "Accounts/";
 const std::string File_ext    = ".json";
 
-class account {
-private:
-
+class account
+{
+   private:
     std::string  type;
     std::string  owner;
     std::string  email;
@@ -18,45 +18,46 @@ private:
     double       interest_rate;
     unsigned int number;
 
-public:
-    account() : balance(0.0) {}
+   public:
+    account() :
+        balance(0.0) {}
 
     // setters:
     void setType(const std::string& t);
-    
+
     void setInterestRate(double r);
-    
+
     void setowner(const std::string& o);
-    
+
     void setBalance(double b);
-    
+
     void setNumber(unsigned int n);
-    
+
     void setEmail(const std::string& e);
-    
+
     void setPassword(std::string& p);
 
-    // json maker 
+    // json maker
     nlohmann::json toJson() const;
 
     // methods
     void store();
-    
+
     void display();
-    
+
     void remove_file();
 
     // getters
     std::string getType();
-    
+
     double getInterestRate();
-    
+
     std::string getOwner();
-    
+
     double getBalance();
-    
+
     unsigned int getNumber();
-    
+
     std::string getEmail();
 };
 
@@ -64,4 +65,4 @@ public:
 std::string generateSalt();
 std::string bcryptHash(const std::string& password, const std::string& salt);
 
-#endif // ACC_HPP
+#endif  // ACC_HPP
