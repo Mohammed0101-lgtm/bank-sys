39d17d8e48248d5e85d3ae453b9248b37ca87267
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-02-17 01:17:43 +0100

    stuff

--- config.hpp
+++ config.hpp
@@ -2,42 +2,41 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
-#include <string>
-#include <iostream>
-#include <vector>
 #include <fstream>
+#include <iostream>
 #include <nlohmann/json.hpp>
+#include <string>
+#include <vector>
 
 #define RED "\x1b[31m"
 #define GREEN "\x1b[32m"
 #define RESET "\x1b[0m"
-#define YELLOW "\x1b[33m">
+#define YELLOW "\x1b[33m" >
 
-static inline const std::string base64_chars = 
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    "abcdefghijklmnopqrstuvwxyz"
-    "0123456789+/";
+static inline const std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                               "abcdefghijklmnopqrstuvwxyz"
+                                               "0123456789+/";
 
-static inline bool is_base64(unsigned char c) {
-    return (isalnum(c) || (c == '+') || (c == '/'));
-}
+static inline bool is_base64(unsigned char c) { return (isalnum(c) || (c == '+') || (c == '/')); }
 
 inline std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
-    std::string ret;
-    int i = 0;
-    int j = 0;
+    std::string   ret;
+    int           i = 0;
+    int           j = 0;
     unsigned char char_array_3[3];
     unsigned char char_array_4[4];
 
-    while (in_len--) {
+    while (in_len--)
+    {
         char_array_3[i++] = *(bytes_to_encode++);
-        if (i == 3) {
+        if (i == 3)
+        {
             char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
             char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
             char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
             char_array_4[3] = char_array_3[2] & 0x3f;
 
-            for (i = 0; (i <4) ; i++)
+            for (i = 0; (i < 4); i++)
                 ret += base64_chars[char_array_4[i]];
             i = 0;
         }
@@ -62,21 +61,24 @@ inline std::string base64_encode(unsigned char const* bytes_to_encode, unsigned 
     return ret;
 }
 
-inline std::string base64_encode(const std::string &input) {
+inline std::string base64_encode(const std::string& input) {
     return base64_encode(reinterpret_cast<const unsigned char*>(input.data()), input.size());
 }
 
 inline std::string base64_decode(std::string const& encoded_string) {
-    int in_len = encoded_string.size();
-    int i = 0;
-    int j = 0;
-    int in_ = 0;
+    int           in_len = encoded_string.size();
+    int           i      = 0;
+    int           j      = 0;
+    int           in_    = 0;
     unsigned char char_array_4[4], char_array_3[3];
-    std::string ret;
+    std::string   ret;
 
-    while (in_len-- && (encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
-        char_array_4[i++] = encoded_string[in_]; in_++;
-        if (i == 4) {
+    while (in_len-- && (encoded_string[in_] != '=') && is_base64(encoded_string[in_]))
+    {
+        char_array_4[i++] = encoded_string[in_];
+        in_++;
+        if (i == 4)
+        {
             for (i = 0; i < 4; i++)
                 char_array_4[i] = base64_chars.find(char_array_4[i]);
 
@@ -86,11 +88,13 @@ inline std::string base64_decode(std::string const& encoded_string) {
 
             for (i = 0; (i < 3); i++)
                 ret += char_array_3[i];
+
             i = 0;
         }
     }
 
-    if (i) {
+    if (i)
+    {
         for (j = i; j < 4; j++)
             char_array_4[j] = 0;
 
@@ -101,22 +105,22 @@ inline std::string base64_decode(std::string const& encoded_string) {
         char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
         char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
 
-        for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
+        for (j = 0; (j < i - 1); j++)
+            ret += char_array_3[j];
     }
 
     return ret;
 }
 
 
-inline std::string get_string(const std::string &prompt) {
+inline std::string get_string(const std::string& prompt) {
     std::cout << prompt << " : ";
     std::string input;
     std::getline(std::cin, input);
 
     size_t len = input.length();
-    if (len > 0 && input[len] == '\n') {
+    if (len > 0 && input[len] == '\n')
         input[len] = '\0';
-    }
 
     return input;
 }
@@ -126,14 +130,13 @@ inline nlohmann::json get_jsonData(const std::string& filename) {
 
     std::ifstream inFile(filename, std::ios::in);
 
-    if (!inFile.is_open()){
+    if (!inFile.is_open())
         throw std::runtime_error("Error opening file");
-    }
 
-    try {   
-        inFile >> data;
-    } catch(const nlohmann::json::parse_error& e) {
-        std::cerr << "Error parsing json file: " << filename <<std::endl;
+    try
+    { inFile >> data; } catch (const nlohmann::json::parse_error& e)
+    {
+        std::cerr << "Error parsing json file: " << filename << std::endl;
         inFile.close();
         exit(EXIT_FAILURE);
     }
